0ccc03769fb46d7d7778c068a6a8e77e3f886fbe
bonchovylkov <bonchovylkov@abv.bg>
2015-05-26 19:03:47 +0300

    Add tick tack toe repo
    Add tick tack toe repo

---
+++ Models/GameOptions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TickTackToe.Models
+{
+    public class GameOptions
+    {
+        public int[,] Field { get; set; }
+        public bool IsEndGame { get; set; }
+
+        public int  WinnerNumber { get; set; }
+    }
+}
---
+++ obj/Release/Package/PackageTmp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TickTackToe.WebApiApplication" Language="C#" %>
